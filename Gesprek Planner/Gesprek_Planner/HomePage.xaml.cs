14c39264230192c370b4ae2c5a5a95795656fa79
David Hijlkema <David.Hijlkema@gmail.com>
2017-02-25 16:12:32 +0100

    Initial commit

---
+++ Gesprek Planner/Gesprek_Planner/HomePage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FormsPlugin.Iconize;
+using ODS_Meester_Aafjes.Interfaces;
+using ODS_Meester_Aafjes.Models;
+using Plugin.Settings;
+using Xamarin.Forms;
+
+namespace ODS_Meester_Aafjes.Pages
+{
+    public partial class HomePage : ContentPage
+    {
+        List<MasterPageItem> GotoPageList = new List<MasterPageItem>();
+        public HomePage()
+        {
+            GotoPageList.Add(new MasterPageItem {Icon = "fa-envelope", TargetType = typeof(Contact), Title="Contact"});
+            InitializeComponent();
+            IconButton twitterButton = new IconButton {Text = "fa-twitter", IsVisible = (bool)App.Database.MetaDataTable.GetValueByKey("twitterActive") };
+            twitterButton.Clicked += GotoTwitterPage;
+            IconButton facebookButton = new IconButton {Text = "fa-facebook", IsVisible = (bool)App.Database.MetaDataTable.GetValueByKey("facebookActive") };
+            facebookButton.Clicked += GotoFacebookPage;
+
+            Grid.Children.Add(twitterButton);
+            Grid.Children.Add(facebookButton);
+        }
+
+        private void GotoTwitterPage(object sender, EventArgs e)
+        {
+            DependencyService.Get<ISocialLinkOpener>().OpenTwitterLink(App.Database.MetaDataTable.GetByKey("twitterAccount").Result.Get());
+        }
+
+        private void GotoFacebookPage(object sender, EventArgs e)
+        {
+            DependencyService.Get<ISocialLinkOpener>().OpenFacebookLink("Ods-Meester-Aafjes-554723344611117");
+        }
+    }
+}
---
+++ Gesprek Planner/Gesprek_Planner/MasterPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using ODS_Meester_Aafjes.Models;
+using ODS_Meester_Aafjes.Pages.Options;
+using Xamarin.Forms;
+using GroupPasswordPage = ODS_Meester_Aafjes.Pages.Options.GroupPasswordPage;
+
+namespace ODS_Meester_Aafjes.Pages
+{
+    public partial class MasterPage : ContentPage
+    {
+        public static Page navigateTo;
+        public ListView MasterPages;
+        public MasterPage()
+        {
+            InitializeComponent();
+            MasterPages = listView;
+            var masterPageItems = new List<MasterPageItem>
+            {
+                new MasterPageItem
+                {
+                    TargetType = typeof(HomePage),
+                    Title = "Home",
+                    Icon = "fa-home"
+                },
+                new MasterPageItem
+                {
+                    TargetType = typeof(GroupPasswordPage),
+                    Title = "Groeps inlog",
+                    Icon = "fa-key"
+                },
+                new MasterPageItem
+                {
+                    TargetType = typeof(Contact),
+                    Title = "Contact",
+                    Icon = "fa-envelope"
+                },
+                new MasterPageItem
+                {
+                    TargetType = typeof(SchoolWiki),
+                    Title = "School wiki",
+                    Icon = "fa-globe"
+                },
+                new MasterPageItem
+                {
+                    TargetType = typeof(OptionsMainPage),
+                    Title = "Opties",
+                    Icon = "fa-gears"
+                }
+            };
+
+            listView.ItemsSource = masterPageItems;
+        }
+    }
+}
