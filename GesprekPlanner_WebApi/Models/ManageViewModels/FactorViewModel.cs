55d297b6c4e90b3a21c468e61b13d09a8395a7c2
StarlightNL <David.Hijlkema@gmail.com>
2017-03-06 07:23:27 +0100

    vacation work

---
+++ GesprekPlanner_WebApi/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GesprekPlanner_WebApi.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
---
+++ GesprekPlanner_WebApi/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GesprekPlanner_WebApi.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
